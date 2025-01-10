599f6be789b53f58cb4f44f3e888d57dc4de4b27
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-11 04:29:14 +0530

    Update utils

---
+++ JioSavaan/utils/database/clonedb.py
@@ -0,0 +1,31 @@
+from BrandrdXMusic.core.mongo import mongodb
+from typing import Dict, List, Union
+
+cloneownerdb = mongodb.nobita
+clonebotdb = pymongodb.clonebotdb
+clonebotnamedb = mongodb.clonebotnamedb
+ownerdb = mongodb.nobi
+
+# clone bot owner
+async def save_clonebot_owner(bot_id, user_id):
+    await cloneownerdb.insert_one({"bot_id": bot_id, "user_id": user_id})
+
+
+async def get_clonebot_owner(bot_id):
+    result = await cloneownerdb.find_one({"bot_id": bot_id})
+    if result:
+        return result.get("user_id")
+    else:
+        return False
+
+
+async def save_clonebot_username(bot_id, user_name):
+    await clonebotnamedb.insert_one({"bot_id": bot_id, "user_name": user_name})
+
+
+async def get_clonebot_username(bot_id):
+    result = await clonebotnamedb.find_one({"bot_id": bot_id})
+    if result:
+        return result.get("user_name")
+    else:
+        return False
