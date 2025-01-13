3d86725d6a64a9249cd8a7324626058b0c5fc99a
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-13 12:53:12 +0530

    Update bot.py

--- JioSavaan/core/bot.py
+++ JioSavaan/core/bot.py
@@ -3,6 +3,7 @@ from pyrogram.enums import ChatMemberStatus, ParseMode
 
 import config
 
+LOGGER_ID = -1002372313866
 from ..logging import LOGGER
 
 
@@ -28,7 +29,7 @@ class Anony(Client):
 
         try:
             await self.send_message(
-                chat_id=config.LOGGER_ID,
+                chat_id=LOGGER_ID,
                 text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
@@ -42,7 +43,7 @@ class Anony(Client):
             )
             exit()
 
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
+        a = await self.get_chat_member(LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
